defb26dfac364ec20e6aed166436275e63fd6ddd
joseph weathington <jweathingtonsr@gmail.com>
2024-12-22 22:00:31 -0600

    stub up js variables

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const cardInfo = document.querySelector("#card");
 const startButton = document.getElementById("start-btn")
-
+const guessText = document.getElementById("guess-text");
+const userInput = document.getElementById("user-input");
+const cardText = document.getElementById("card-text")
 
 startButton.onclick = function () {
-    console.log("Hello it's working")
+
 }
