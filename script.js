56df7321e575a64ef49882e5145d7b7b6f6c9876
joseph weathington <jweathingtonsr@gmail.com>
2024-12-22 22:29:11 -0600

    create reset button to remove guess number text

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const cardInfo = document.querySelector("#card");
 const startButton = document.getElementById("start-btn")
+const resetButton = document.getElementById("reset-btn")
 const guessText = document.getElementById("guess-text");
 const userInput = document.getElementById("user-input");
 const cardText = document.getElementById("card-text")
 
 startButton.onclick = function () {
+guessText.style.display = "block"
+}
 
+resetButton.onclick = function () {
+    guessText.style.display = "none"
 }
