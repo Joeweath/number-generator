04e127988f228f1aba3a5cb4a500137fd0076a72
joseph weathington <jweathingtonsr@gmail.com>
2024-12-23 20:17:20 -0600

    stub up resetbutton functionalities and create random number for card text

--- script.js
+++ script.js
@@ -4,15 +4,38 @@ const resetButton = document.getElementById("reset-btn")
 const guessText = document.getElementById("guess-text");
 const userInput = document.getElementById("user-input");
 const cardText = document.getElementById("card-text")
+let currentNum;
+let currentInputValue = ""
+
+
 
 startButton.onclick = function () {
-guessText.style.display = "block"
+    startGame()
 }
 
 resetButton.onclick = function () {
     guessText.style.display = "none"
+    userInput.value = ""
+    cardText.innerText = "?"
+    cardText.style.fontSize = "10rem"
 }
 
 userInput.addEventListener('input', (event) => {
     console.log(event.target.value)
+    currentInputValue = event.target.value
 })
+
+const randomNumber = () => {
+    currentNum = Math.floor(Math.random()* 101)
+    if(currentNum.toString().length > 1){
+        cardText.style.fontSize = "7rem"
+    }
+    return currentNum
+}
+
+const startGame = () =>{
+    resetButton.style.display = "inline"
+    guessText.style.display = "block"
+    randomNumber()
+    cardText.innerText = currentNum
+}
